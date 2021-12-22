a52e3915f49d3947ea2baaf815c0158af242b12f
Mayura0411 <96528334+Mayura0411@users.noreply.github.com>
2021-12-22 17:32:14 +0530

    Add files via upload

---
+++ simplecalculator.py
@@ -0,0 +1,13 @@
+print("simple calculator")
+a=5
+b=3
+sum=a+b
+subtraction=a-b
+divide=a/b
+multiply=a*b
+remainder=a%b
+print("sum of a and b is",sum)
+print("if you subtract b from a the answer will be",subtraction)
+print("a divided by b is",divide)
+print("if you multiply a and b the answer will be",multiply)
+print("the remainder of a and b is",remainder)
